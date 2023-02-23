2aef90800e5cceca87ed90098a7a1243ca268e3a
mssalunkhe <70637458+mssalunkhe@users.noreply.github.com>
2023-02-23 17:03:14 +0530

    Update PalindromeMinCut.java

--- PalindromeMinCut.java
+++ PalindromeMinCut.java
@@ -55,6 +55,57 @@ public class PalindromeMinCut {
             }
         }
     }
+    
+        public int findMinCut_Vers1(String str){
+        int len=str.length();
+        endIndex=len-1;
+        int [] possibleCutPositions= new int[len];
+        int [] minCuts= new int[len];
+        int i,j,k, toReturn =str.length()+1;
+
+        if( len==0 ) return -1;
+        if( len==1 ) return 0;
+        char c=str.charAt(0);
+
+        //find possible cut positions
+        for ( i = 0,j=0; i <len ; ) {
+            int index=str.indexOf(c,i);
+            if(index==-1) break;
+            else{
+                possibleCutPositions[j++]=index;
+                i=index+1;
+
+            }
+
+        }
+        //-1 to indicate no more possible positions
+        possibleCutPositions[j]=-1;
+        j=0;
+        // -2 indicates the cut position is not a palindrome
+        while(possibleCutPositions[j]!=-1){
+            String substring=str.substring(0,possibleCutPositions[j]+1);
+            if(! isPalindrome(substring))
+                possibleCutPositions[j]=-2;
+            j++;
+        }
+
+        //find mincuts of the valid strings
+        for ( k = 0; possibleCutPositions[k]!=-1  ; k++) {
+            if(possibleCutPositions[k]!=-2)
+                minCuts[k]=1+findMinCut_Vers1(str.substring(possibleCutPositions[k]+1));
+            else minCuts[k]=str.length()+1;
+        }
+
+        for (int l = 0; l <k ; l++) {
+            if(minCuts[l]< toReturn) toReturn =minCuts[l];
+        }
+        return toReturn;
+
+
+
+
+
+    }
     public boolean isPalindrome(String str){
         int length=str.length();
         int mid=(length+1)/2;
